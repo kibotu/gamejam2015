029bfbef940fbe2eabde1226aab543de70791768
Sprotte <sprottemail@googlemail.com>
2015-01-24 14:01:43 +0100

    LocalScale

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,40 +3,52 @@ using System.Collections;
 
 public class Character : MonoBehaviour
 {
-
-    public InputController input;
-    public float speed;
-
-    void Start()
-    {
-        input.Keyboard1Up += Up;
-        input.Keyboard1Down += Down;
-        input.Keyboard1Left += Left;
-        input.Keyboard1Right += Right;
-    }
-
-    void Up()
-    {
-        transform.Translate(0, speed * Time.deltaTime, 0);
-    }
-
-    void Down()
-    {
-        transform.Translate(0, -speed * Time.deltaTime, 0);
-    }
-
-    void Left()
-    {
-        transform.Translate(-speed * Time.deltaTime, 0, 0);
-    }
-
-    void Right()
-    {
-        transform.Translate(speed * Time.deltaTime, 0, 0);
-    }
-
-    void Update()
-    {
-
-    }
+	
+	public InputController input;
+	public float speed;
+	public bool facingRight = true;
+	
+	void Start()
+	{
+		input.Keyboard1Up += Up;
+		input.Keyboard1Down += Down;
+		input.Keyboard1Left += Left;
+		input.Keyboard1Right += Right;
+	}
+	
+	void Up()
+	{
+		transform.Translate(0, speed * Time.deltaTime, 0);
+	}
+	
+	void Down()
+	{
+		transform.Translate(0, -speed * Time.deltaTime, 0);
+	}
+	
+	void Left()
+	{
+		if(facingRight)
+			Flip();
+		transform.Translate(-speed * Time.deltaTime, 0, 0);
+	}
+	
+	void Right()
+	{
+		if(!facingRight)
+			Flip();
+		transform.Translate(speed * Time.deltaTime, 0, 0);
+	}
+	
+	void Update()
+	{
+		
+	}
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector2 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
 }
