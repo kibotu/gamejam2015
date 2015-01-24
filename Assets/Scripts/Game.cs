a7ff84ec34a21b4f02e447e12800a7cdd945f273
Jan Rabe <janrabe@kibotu.net>
2015-01-24 17:14:00 +0100

    added network handler stub

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour
+{
+    public NetworkHandler network;
+
+	void Start ()
+	{
+	    network.OnConnect += OnConnect;
+
+	}
+
+    void OnConnect(int id, string name, byte action)
+    {
+        
+    }
+	
+	void Update () {
+	
+	}
+}
