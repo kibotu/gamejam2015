18c4190b448a5944fcf3394a804d90f04a314d50
Sprotte <sprottemail@googlemail.com>
2015-01-25 05:59:44 +0100

    more

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -3,23 +3,44 @@ using System.Collections;
 using System.Linq;
 using System.Collections.Generic;
 
+using watdowedonow;
+
+
 public class GameMode : MonoBehaviour {
 
+    public enum GameType { Struggle = 0, };
+    public GameType currentGameMode = GameType.Struggle;
+    public Game game;
     public void GiveWinner()
     {
-        //GameObejct[] players = GameObject.FindGameObjectsWithTag("Player");
-        Dictionary<int,GameObject> playerStats = new Dictionary<int, GameObject>();
-        GameObject[] players  = GameObject.FindGameObjectsWithTag("Player");
-        GameObject closest;
-        foreach (GameObject player in players)
+        switch (currentGameMode)
         {
-            playerStats.Add(player.GetComponent<PlayerStats>().kills,player);
-        }
-        List<int> keyList = playerStats.Keys.ToList();
-        keyList.Sort();
+            case GameType.Struggle:
+                Dictionary<int,GameObject> playerStats = new Dictionary<int, GameObject>();
+                GameObject[] players  = GameObject.FindGameObjectsWithTag("Player");
+                GameObject closest;
+                foreach (GameObject player in players)
+                {
+                    playerStats.Add(player.GetComponent<PlayerStats>().kills,player);
+                }
+                List<int> keyList = playerStats.Keys.ToList();
+                keyList.Sort();
+                
+                GameObject winner = playerStats[keyList.Last()];
+                
+                print("Winner is Player: "+ winner.name+" with "+keyList.Last()+" Kills");
+                switchGameMode();
+                break;
+        }    
 
-        GameObject winner = playerStats[keyList.Last()];
 
-        print("Winner is Player: "+ winner.name+" with "+keyList.Last()+" Kills");
     }
+
+    void switchGameMode()
+    {
+        currentGameMode = GameType.Struggle;
+        game.SECONDS_LEFT = 15;
+        game.showResult = true;
+    }
+
 }
---
+++ Assets/Scripts/HealthbarSlider.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthbarSlider : MonoBehaviour {
+    
+    [Range(0, 1)]
+    public float HealthPercentage;
+    
+    public Image Fill;
+    public Image Container;
+    
+    public void SetHealthBar(float percentage) 
+    {
+        HealthPercentage = percentage;
+        Fill.rectTransform.sizeDelta = new Vector2( HealthPercentage * 826, Fill.rectTransform.sizeDelta.y); 
+    }
+}
